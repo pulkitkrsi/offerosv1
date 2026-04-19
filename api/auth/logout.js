8f5b61c9cd3b485a061400091721ec4c9317b9a8
pulkitkrsi <pulkit.singh@chargezone.co.in>
2026-04-19 14:07:22 +0530

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-import { clearCookie } from "../lib/auth.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-  clearCookie(res);
-  return res.status(200).json({ ok: true });
-}
